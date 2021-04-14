38400e55ef4ad845b3cc9f3fc2f823dea7365824
dodoongtak <themighty.tak@gmail.com>
2021-04-14 11:51:52 +0900

    🧐 data: Update Apartment types

--- src/fixtures/categories.js
+++ src/fixtures/categories.js
@@ -11,18 +11,18 @@ const categories = {
       keyword: '서울',
     },
     {
+      address: 11170,
+      year: 202103,
+      keyword: '래미안첼리투스아파트',
+    },
+    {
       address: 11680,
       year: 202102,
       keyword: '한양1차(영동한양)',
     },
-    {
-      address: 11200,
-      year: 202103,
-      keyword: '트리마제',
-    },
   ],
 
-  businessdistrict: [
+  commercial: [
     {
       address: 11650,
       year: 202102,
@@ -42,6 +42,98 @@ const categories = {
       address: 11440,
       year: 202102,
       keyword: '마포래미안푸르지오1단지',
+    },
+  ],
+
+  landscape: [
+    {
+      address: 11650,
+      year: 202102,
+      keyword: '반포자이',
+    },
+    {
+      address: 11650,
+      year: 202102,
+      keyword: '래미안퍼스티지',
+    },
+    {
+      address: 11650,
+      year: 202103,
+      keyword: '반포리체',
+    },
+    {
+      address: 11440,
+      year: 202102,
+      keyword: '마포래미안푸르지오4단지',
+    },
+  ],
+
+  commute_gangnam: [
+    {
+      address: 11200,
+      year: 202103,
+      keyword: '갤러리아 포레',
+    },
+    {
+      address: 11200,
+      year: 202102,
+      keyword: '아크로 서울포레스트',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '타워팰리스2',
+    },
+    {
+      address: 11650,
+      year: 202102,
+      keyword: '반포자이',
+    },
+  ],
+
+  commute_yeouido: [
+    {
+      address: 11650,
+      year: 202102,
+      keyword: '반포자이',
+    },
+    {
+      address: 11650,
+      year: 202102,
+      keyword: '반포 주공1단지',
+    },
+    {
+      address: 11650,
+      year: 202102,
+      keyword: '래미안퍼스티지',
+    },
+    {
+      address: 11650,
+      year: 202103,
+      keyword: '아크로리버파크',
+    },
+  ],
+
+  commute_gwanghwamun: [
+    {
+      address: 11560,
+      year: 202102,
+      keyword: '서울',
+    },
+    {
+      address: 11560,
+      year: 202103,
+      keyword: '시범',
+    },
+    {
+      address: 11560,
+      year: 202102,
+      keyword: '여의도자이',
+    },
+    {
+      address: 11560,
+      year: 202102,
+      keyword: '광장',
     },
   ],
 };
