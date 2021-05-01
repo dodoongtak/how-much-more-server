8e3a1ae21c27036c935c53d15eac94cf3b87754e
dodoongtak <themighty.tak@gmail.com>
2021-05-02 01:24:23 +0900

    🧐 data: Add new apartment categories
    New Categories are the followings:
    - ocean
    - rank
    - bts
    - celebrity
    - penthouse

--- src/fixtures/categories.js
+++ src/fixtures/categories.js
@@ -22,6 +22,181 @@ const categories = {
     },
   ],
 
+  ocean: [
+    {
+      address: 42210,
+      year: 202104,
+      keyword: '속초자이',
+    },
+    {
+      address: 26350,
+      year: 202009,
+      keyword: '엘시티',
+    },
+    {
+      address: 26350,
+      year: 202011,
+      keyword: '더샵센텀스타',
+    },
+    {
+      address: 26350,
+      year: 202101,
+      keyword: '해운대두산위브더제니스',
+    },
+    {
+      address: 26350,
+      year: 202012,
+      keyword: '해운대 I PARK',
+    },
+  ],
+
+  rank: [
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '더펜트하우스청담',
+    },
+    {
+      address: 11680,
+      year: 202104,
+      keyword: '현대7차(73~77,82,85동)',
+    },
+    {
+      address: 11170,
+      year: 202103,
+      keyword: '한남더힐',
+    },
+    {
+      address: 11170,
+      year: 202103,
+      keyword: '나인원 한남',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '현대2차(10,11,20,23,24,25동)',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '현대6차(78~81,83,84,86,87동)',
+    },
+    {
+      address: 11200,
+      year: 202103,
+      keyword: '갤러리아 포레',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '상지리츠빌 카일룸',
+    },
+    {
+      address: 11200,
+      year: 202104,
+      keyword: '아크로 서울포레스트',
+    },
+  ],
+
+  bts: [
+    {
+      address: 11170,
+      year: 202103,
+      keyword: '한남더힐',
+    },
+    {
+      address: 11170,
+      year: 202006,
+      keyword: '리버힐삼성',
+    },
+    {
+      address: 11680,
+      year: 202008,
+      keyword: '아펠바움',
+    },
+    {
+      address: 11650,
+      year: 202104,
+      keyword: '반포 주공1단지',
+    },
+    {
+      address: 11200,
+      year: 202104,
+      keyword: '트리마제',
+    },
+  ],
+
+  celebrity: [
+    {
+      address: 11680,
+      year: 202104,
+      keyword: '현대7차(73~77,82,85동)',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '삼성동힐스테이트 1단지',
+    },
+    {
+      address: 11440,
+      year: 202102,
+      keyword: '한강밤섬자이',
+    },
+    {
+      address: 11440,
+      year: 202102,
+      keyword: '신구강변연가',
+    },
+    {
+      address: 11200,
+      year: 202104,
+      keyword: '트리마제',
+    },
+    {
+      address: 11590,
+      year: 202006,
+      keyword: 'MARKHILLS',
+    },
+    {
+      address: 41111,
+      year: 202104,
+      keyword: '수원 SK SKY VIEW',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '삼성동힐스테이트 1단지',
+    },
+  ],
+
+  penthouse: [
+    {
+      address: 28185,
+      year: 202103,
+      keyword: '송도아트윈푸르지오',
+    },
+    {
+      address: 11440,
+      year: 201603,
+      keyword: '상암카이저팰리스클래식',
+    },
+    {
+      address: 11650,
+      year: 202012,
+      keyword: '노블씨티',
+    },
+    {
+      address: 11680,
+      year: 202103,
+      keyword: '더펜트하우스청담',
+    },
+    {
+      address: 11170,
+      year: 202103,
+      keyword: '나인원 한남',
+    },
+  ],
+
   commercial: [
     {
       address: 11650,
